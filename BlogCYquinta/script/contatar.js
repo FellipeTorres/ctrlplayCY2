6737a6951068dc680bcc7596a185c2ddca4452c4
FellipeTorres <fellipetorres38@gmail.com>
2025-05-15 20:58:58 -0300

    Add files via upload

---
+++ BlogCYquinta/script/contatar.js
@@ -0,0 +1,13 @@
+let nomes = [];
+let emailo = [];
+let assuntos = [];
+let mensagens = [];
+
+function contato(nome, email, assunto, mensagem){
+    nomes.push(nome);
+    emailo.push(email);
+    assuntos.push(assunto);
+    mensagens.push(mensagem);
+
+    return ""
+}
