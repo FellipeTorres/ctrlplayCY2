1bc5e22b021add46f895eb90c30cfc13ef587d54
FellipeTorres <fellipetorres38@gmail.com>
2025-05-22 21:07:44 -0300

    Add files via upload

--- BlogCYquinta/script/contatar.js
+++ BlogCYquinta/script/contatar.js
@@ -3,11 +3,42 @@ let emailo = [];
 let assuntos = [];
 let mensagens = [];
 
-function contato(nome, email, assunto, mensagem){
+let tabela = "";
+
+function contato(event) {
+
+    event.preventDefault();
+
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let assunto = document.getElementById('assunto').value;
+    let mensagem = document.getElementById('mensagem').value;
+
     nomes.push(nome);
     emailo.push(email);
     assuntos.push(assunto);
     mensagens.push(mensagem);
 
-    return ""
+    tabela = `<tr>
+            <th>Nomes</tr>
+            <th>emailo</th>
+            <th>assuntos</tr>
+            <th>mensagens</th>
+            </tr>`;
+
+    for (let i = 0; i < nomes.length; 1++) {
+
+        tabela += `<tr>
+                    <th>${nomes[i]}nomes</tr>
+                    <th>${emailo[i]}emailo</th>
+                    <th>${assuntos[i]}assuntos</tr>
+                    <th>${mensagens[i]}mensagens</th>
+                </tr>`;
+
+    }
+
+
+    document.getElementById('BD').innerHTML = tabela;
+
+    return false;
 }
